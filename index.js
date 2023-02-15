5a497d353f5972caac0daa71f471554f2bfe47c7
Vinícius Campos <viniciuscampos1408@gmail.com>
2023-02-15 18:33:03 -0300

    remove router express

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const { Client, Collection, Events, GatewayIntentBits, InteractionType, ActivityType } = require('discord.js');
-const axios = require('axios')
-const express = require('express');
 const client = new Client({ intents: ["Guilds", "GuildMembers", "MessageContent", "GuildMessages"] });
 require('dotenv').config()
 module.exports = client;
@@ -19,24 +17,6 @@ client.on(Events.InteractionCreate, interaction => {
     }
 })
 
-const app = express();
-// app.use(bodyParser.json());
-
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${process.env.ACCESS_TOKEN}`
-  }
-});
-
-app.get('/', async (req,res) =>{
-    return res.send('Follow documentation ')
-  })
-
 client.on('ready', () => {
     console.log(`${client.user.username} está online!`);
 
@@ -49,7 +29,3 @@ client.on('ready', () => {
 client.slashCommands = new Collection();
 require('./src/handler')(client);
 client.login(process.env.ACCESS_TOKEN);
-
-app.listen(8999, () => {
-
-})
