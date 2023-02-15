49596373771e2549972fabdd22fee41eda374b84
Vinícius Campos <viniciuscampos1408@gmail.com>
2023-02-15 14:30:09 -0300

    Versão 1.0.1 - Ajustes nas pastas, chamada de metodo e acesso ao token

---
+++ src/handler/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+
+module.exports = async (client) => {
+
+    const SlashsArray = []
+
+    fs.readdir(`./src/commands`, (error, folder) => {
+        folder.forEach(subfolder => {
+            fs.readdir(`./src/commands/${subfolder}/`, (error, files) => {
+                files.forEach(files => {
+
+                    if (!files?.endsWith('.js')) return;
+                    files = require(`../commands/${subfolder}/${files}`);
+                    if (!files?.name) return;
+                    client.slashCommands.set(files?.name, files);
+
+                    SlashsArray.push(files)
+                });
+            });
+        });
+    });
+    client.on("ready", async () => {
+        client.guilds.cache.forEach(guild => guild.commands.set(SlashsArray))
+    });
+};
+
