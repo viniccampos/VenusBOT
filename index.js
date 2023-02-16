dfb62a367e64401bab2818d5fc2efa13a6df6ca6
Vinícius Campos <viniciuscampos1408@gmail.com>
2023-02-16 01:34:04 -0300

    Inicio design, add routes, views, public e ejs

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const Router = require('express');
 const app = express();
-const route = Router();
-require('./src/bot/app.js')
-require('dotenv').config()
-
-route.get("/", (req, res) => {
-    res.send("VenusBot");
-    /*return res.json({
-        success: true,
-        message: "VênusBOT on"
-    });*/
-});
+const path = require('path');
+const routes = require('./src/routes/routes');
+require('./src/bot/app');
+require('dotenv').config();
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, './src/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 const PORT = process.env.PORT || 5000;
-
-app.use(route);
 
 app.listen(PORT, () => {
     console.log("Servidor Online na porta: " + PORT);
